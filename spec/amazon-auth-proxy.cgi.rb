53fcea25349c36bd3dc0a7037f980b3b43e077e7
TADA Tadashi <t@tdtds.jp>
2011-09-29 17:09:56 +0900

    added specs.

---
+++ spec/amazon-auth-proxy.cgi.rb
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+load File.expand_path('../../amazon-auth-proxy.cgi', __FILE__)
+require 'date'
+
+describe 'paapi' do
+	before :each do
+		@conf = {
+			'access_key' => 'SAMPLE_ACCESS_KEY',
+			'secret_key' => 'SAMPLE_SECRET_KEY',
+			'entry_point' => 'http://webservices.amazon.co.jp/onca/xml',
+			'xslt_entry_point' => 'http://xml-jp.amznxslt.com/onca/xml',
+			'default_aid' => 'cshs-22',
+			'use_redirect' => true,
+		}
+
+		@req = {
+			'Service' => ['AWSECommerceService'],
+			'SubscriptionId' => ['12345678901234567890'],
+			'Version' => ['2007-10-29'],
+			'Operation' => ['ItemSearch'],
+			'ResponseGroup' => ['Small'],
+			'SearchIndex' => ['Books'],
+			'Keywords' => ['Amazon'],
+			'ItemPage' => ['1'],
+		}
+	end
+
+	context '正常系' do
+		it '返り値が2要素からなる' do
+			paapi( @conf, @req ).should have(2).items
+		end
+
+		it '返り値の第1要素(レスポンドコード)が302' do
+			paapi( @conf, @req )[0].should eq 302
+		end
+
+		it '返り値の第2要素(redirect先)が正しいAWSリクエスト' do
+			paapi( @conf, @req )[1].should match %r|\Ahttp://webservices.amazon.co.jp/onca/xml\?AssociateTag=cshs-22&ItemPage=1&Keywords=Amazon&Operation=ItemSearch&ResponseGroup=Small&SearchIndex=Books&Service=AWSECommerceService&SubscriptionId=SAMPLE_ACCESS_KEY&Timestamp=|
+		end
+
+		it 'Styleを指定するとXSLTを使う' do
+			@req['Style'] = ['dummy']
+			paapi( @conf, @req )[1].should match %r|\Ahttp://xml-jp.amznxslt.com/onca/xml\?AssociateTag=cshs-22&ItemPage=1&Keywords=Amazon&Operation=ItemSearch&ResponseGroup=Small&SearchIndex=Books&Service=AWSECommerceService&Style=dummy&SubscriptionId=SAMPLE_ACCESS_KEY&Timestamp=|
+		end
+
+		it 'AssociateTagを指定するとそれを使う' do
+			@req['AssociateTag'] = ['sample-22']
+			paapi( @conf, @req )[1].should match %r|\Ahttp://webservices.amazon.co.jp/onca/xml\?AssociateTag=sample-22&ItemPage=1&Keywords=Amazon&Operation=ItemSearch&ResponseGroup=Small&SearchIndex=Books&Service=AWSECommerceService&SubscriptionId=SAMPLE_ACCESS_KEY&Timestamp=|
+		end
+	end
+
+	context '異常系' do
+	end
+end
