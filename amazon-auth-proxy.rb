b04e846fc7f131258193ef839e9b10622423e156
TADA Tadashi <t@tdtds.jp>
2011-10-05 21:46:15 +0900

    supported multiple assiciate tag for each countory.

--- amazon-auth-proxy.rb
+++ amazon-auth-proxy.rb
@@ -20,7 +20,7 @@ class AmazonAuthProxyApp < Sinatra::Base
 		conf['secret_key'] = @conf['secret_key']
 		conf['entry_point'] = @conf['entry_point'][country]
 		conf['xslt_entry_point'] = @conf['xslt_entry_point'][country]
-		conf['default_aid'] = @conf['default_aid']
+		conf['default_aid'] = @conf['aid'][country]
 		conf['use_redirect'] = true
 		conf
 	end
@@ -30,7 +30,11 @@ class AmazonAuthProxyApp < Sinatra::Base
 		params.each do |k, v|
 			aparams[k] = [v] unless k == 'splat'
 		end
-		status, body = paapi( make_conf( params[:splat][0] ), aparams )
+		begin
+			status, body = paapi( make_conf( params[:splat][0] ), aparams )
+		rescue ArgumentError
+			return 400, "400 Bad request: #{$!}"
+		end
 		redirect body, 302
 	end
 end
